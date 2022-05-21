94537a9a04b9c3d582642b6e717bb83fd89052d5
LimiyaKurian <105207773+LimiyaKurian@users.noreply.github.com>
2022-05-21 23:45:42 +0530

    Update stats.c

--- stats.c
+++ stats.c
@@ -2,9 +2,37 @@
 
 struct Stats compute_statistics(const float* numberset, int setlength) {
     struct Stats s;
-    s.average = 0;
-    s.min = 0;
-    s.max = 0;
+    s.average = 0.0;
+    s.min = numberset[0];
+    s.max = 0.0;
+    
+    float sum = 0.0;
+    
+    if ((setlength != 0) && (numberset !=0))
+    {
+        for (int i =0 ; i< setlength; i++)
+        {
+            if (s.max < numberset[i])
+            {
+                s.max = numberset[i];
+            }
+            if (s.min >= numberset[i])
+            {
+                s.min = numberset[i];
+            }
+            sum += number[i];
+        }
+        
+        s.average = sum/(float)setlength;
+        return s;
+    }
+    
+    else
+    {
+        s.average = sum/(float)setlength;
+        s.min += s.average;
+        s.max += s.average;
+    }
 }
 
 int emailAlertCallCount = 0;
